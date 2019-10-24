5e6e87ad356761655e6eac59a3c9bbaf2f0f8d7b
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-10-24 21:53:58 +0700

    update user typing

--- src/typings/models/user.d.ts
+++ src/typings/models/user.d.ts
@@ -1,10 +1,14 @@
 export interface User {
     id: string;
+    firebase_id: string;
     name: string;
+    phone: string;
     username: string;
+    email: string;
     password: string;
-    refresh_token: string;
-    token_validity: string;
+    gender: string;
+
+    image_url: string;
     created_at: string;
     updated_at: string;
     deleted_at: string;
